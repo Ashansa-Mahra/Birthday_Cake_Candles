3fa6aa11a43462268b79246a88ef14cecf4bd48a
Ashansa Mahra <mahraashansa1997@gmail.com>
2024-01-03 19:27:47 +0530

    Add files via upload

---
+++ Birthday_Cake_Candles/Program.cs
@@ -0,0 +1,25 @@
+﻿class Result
+{
+    public static int birthdayCakeCandles(List<int> candles)
+    {
+        var maxHeight = candles.Max();
+        var count = candles.Count(h => h == maxHeight);
+        return count;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        int candlesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> candles = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(candlesTemp => Convert.ToInt32(candlesTemp)).ToList();
+
+        int result = Result.birthdayCakeCandles(candles);
+
+        Console.WriteLine(result);
+    }
+}
